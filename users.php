f9e216aacf3b4b1f30e64d5bfc6b98ad03f4fd2e
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2023-11-22 10:38:07 +0000

    show name

--- users.php
+++ users.php
@@ -24,5 +24,16 @@
         <input type="radio" name="role" value="Admin"> Admin<br>
         <input type="submit" value="Add User">
     </form>   
+    <?php
+	include_once('connection.php');
+
+	$stmt = $conn->prepare("SELECT * FROM tblusers");
+	$stmt->execute();
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+		{
+			echo($row["Forename"].' '.$row["Surname"]."<br>");
+		}
+    ?>  
 </body>
 </html>
