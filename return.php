5917ac9e879ba9c69c7374f19a9e748ed8ce3fee
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2024-01-08 22:11:16 +0000

    return book

---
+++ return.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Borrow</title>
+    
+</head>
+<body>
+
+<form action="assignbook.php" method = "post">
+	<select name = "student">
+
+	<?php
+	include_once('connection.php');
+	$stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC");
+	$stmt->execute();
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+	{
+		echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
+	}
+	?>
+
+	</select>
+
+	<select name = "book">
+
+	<?php
+	include_once('connection.php');
+	$stmt = $conn->prepare("SELECT * FROM Tblbooks WHERE InLibrary = '1' ORDER BY Bookname ASC");
+	$stmt->execute();
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+	{
+		echo('<option value='.$row["BookID"].'>'.$row["BookName"].', '.$row["AuthorSurname"].', '.$row["AuthorForename"].'</option>');
+	}
+	?>
+
+	</select>
+
+	<input type="submit" value="Return">
+
+</form>
+
+	<?php
+        include_once('connection.php');
+
+		$stmt = $conn->prepare("SELECT * FROM TblUsers");
+		$stmt->execute();
+		$stmt = $conn->prepare("SELECT * FROM TblBooks");
+		$stmt->execute();
+        $stmt = $conn->prepare("SELECT * FROM TblBorrow");
+        $stmt->execute();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+            {
+                echo($row["UserID"].' '.$row["BookID"]."<br>");
+            }
+    ?>   
+	
+</body>
+</html>
--- unassignbook.php
+++ unassignbook.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Location:borrow.php');
+header('Location:return.php');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
