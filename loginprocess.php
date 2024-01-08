545eabc870d95b331efd73e16dd9e95b62c7340e
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2024-01-08 21:48:36 +0000

    login

--- loginprocess.php
+++ loginprocess.php
@@ -8,6 +8,7 @@
 
     $stmt = $conn->prepare("SELECT * FROM tblusers WHERE Forename =:username ;" );
     $stmt->bindParam(':username', $_POST['Username']);
+    $attempt = $_POST['Password']
     $stmt->execute();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
